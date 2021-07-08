0e5a4810a551d87160a5068daf886108c0fdbb00
Iliya-Yadegari <naruto3@email.com>
2021-07-08 12:06:38 +0100

    Create RomanNumeralConverter.py

---
+++ RomanNumeralConverter.py
@@ -0,0 +1,26 @@
+num = int(input('Enter a number: '))
+
+if num > 10 or num < 1:
+    print('Your number must not be below 1 or above 10')
+else:
+    if num == 1:
+        print('The Roman numeral is I')
+    elif num == 2:
+        print('The Roman numeral is II')
+    elif num == 3:
+        print('The Roman numeral is III')
+    elif num == 4:
+        print('The Roman numeral is IV')
+    elif num == 5:
+        print('The Roman numeral is V')
+    elif num == 6:
+        print('The Roman numeral is VI')
+    elif num == 7:
+        print('The Roman numeral is VII')
+    elif num == 8:
+        print('The Roman numeral is VIII')
+    elif num == 9:
+        print('The Roman numeral is IX')
+    elif num == 10:
+        print('The Roman numeral is X')
+        
