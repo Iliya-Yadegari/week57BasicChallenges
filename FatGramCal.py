bb315178b6331f67dd80bb4cb5d3120aef60b66d
Iliya-Yadegari <naruto3@email.com>
2021-07-10 20:21:52 +0100

    Update FatGramCal.py

--- FatGramCal.py
+++ FatGramCal.py
@@ -1,13 +1,20 @@
+import sys
+
 calories = int(input("Enter number of calories: "))
 fats = int(input("Enter number of fat in grams: "))
+
 if(calories > 0 and fats > 0):
    cf = fats * 9
-   if(cf < calories):
-       percent = cf / calories  
-       if(percent < 0.3):
-           print("Food is low in fat")
-   else:
-       print("Error input. Calories from fat cannot more than total calories")
-else:
-   print("Error input. Calories and fats must be more than 0")
    
+else:
+    print('The calories or/and the fats cannot be below 0 try again')
+    sys.exit()
+
+if(cf < calories):
+       percent = cf / calories  
+else:
+    print("Error input. Calories from fat cannot more than total calories")
+    sys.exit()
+
+if(percent < 0.3):
+    print("Food is low in fat")
